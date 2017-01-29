cd0d70a22151961a173e85de82b1f93071044796
satyajeet.gawas <satyajeet.gawas@gmail.com>
2017-01-29 14:02:12 -0500

    'key'

--- ble/node-bleacon/StickerScan.js
+++ ble/node-bleacon/StickerScan.js
@@ -15,32 +15,34 @@ EstimoteSticker.on('discover', function(estimoteSticker) {
 
 
 
-var values = "{
-				id:"+estimoteSticker.id+","+
-				"uuid:"+estimoteSticker.uuid+","+
-				"major:"+estimoteSticker.major+","+
-				"minor:"+estimoteSticker.minor+","+
-				"type:"+estimoteSticker.type+","+
-				"firmware:"+estimoteSticker.firmware+","+
-				"bootloader:"+estimoteSticker.bootloader+","+
-				"temperature:"+estimoteSticker.temperature+","+
-				"moving:"+estimoteSticker.moving+","+
-				"batteryLevel:"+estimoteSticker.batteryLevel+","+
-				"x:"+estimoteSticker.acceleration.x+","+
-				"y:"+estimoteSticker.acceleration.y+","+
-				"z:"+estimoteSticker.acceleration.z+","+
-				"currentMotionStateDuration:"+estimoteSticker.currentMotionStateDuration+","+
-				"prevMotionStateDuration:"+estimoteSticker.previousMotionStateDuration+","+
-				"power:"+estimoteSticker.power+","+
-				"firmwareState:"+estimoteSticker.firmwareState+","+
-				"rssi:"+estimoteSticker.rssi+","+
-				"log_time:"+Date.now()+","+
-				"mac_id:"+macAddr+"}";
+var values =  {
+				id:estimoteSticker.id,
+				uuid:estimoteSticker.uuid,
+				major:estimoteSticker.major,
+				minor:estimoteSticker.minor,
+				type:estimoteSticker.type,
+				firmware:estimoteSticker.firmware,
+				bootloader:estimoteSticker.bootloader,
+				temperature:estimoteSticker.temperature,
+				moving:estimoteSticker.moving,
+				batteryLevel:estimoteSticker.batteryLevel,
+				x:estimoteSticker.acceleration.x,
+				y:estimoteSticker.acceleration.y,
+				z:estimoteSticker.acceleration.z,
+				currentMotionStateDuration:estimoteSticker.currentMotionStateDuration,
+				prevMotionStateDuration:estimoteSticker.previousMotionStateDuration,
+				power:estimoteSticker.power,
+				firmwareState:estimoteSticker.firmwareState,
+				rssi:estimoteSticker.rssi,
+				log_time:Date.now(),
+				mac_id:macAddr
+			};
 
 				var sa = require('superagent');
 				sa.post('http://198.211.106.52/crc/saveEstimotedata')
   				.send(values)
-  				.end(function(err, res) {
+  				.end(function(err,
+  						 res) {
     					console.log(err);
     					console.log(res);
   				});
