893a1ad97695b29f1659982a2e4df4f59816d0cf
satyajeet.gawas <satyajeet.gawas@gmail.com>
2017-01-29 13:55:44 -0500

    updated with api

--- ble/node-bleacon/StickerScan.js
+++ ble/node-bleacon/StickerScan.js
@@ -1,29 +1,13 @@
 
 var EstimoteSticker = require('./estimote-sticker');
 
-		var connection = mysql.createConnection(
-			{
-				connectionLimit:1000,
-				connectTimeout:60*60*1000,
-				acquireTimeout:60*60*1000,
-				timeout:60*60*1000,
-				host:'198.211.106.52',
-				user : 'gtlyfe',
-				password : 'gt123@lyfe',
-				port : 3306,
-				database : 'estimote',
-			});
-
-		connection.connect(function(err){
-		/*console.log(err);*/
-});
 
 		
 EstimoteSticker.on('discover', function(estimoteSticker) {
   	
 		console.log(estimoteSticker);
 		
-		var mysql = require('mysql');
+		
 
 		var getmac=require('getmac');
 		getmac.getMac(function(err,macAddr){
@@ -31,31 +15,39 @@ EstimoteSticker.on('discover', function(estimoteSticker) {
 
 
 
-var values = {id:estimoteSticker.id,uuid:estimoteSticker.uuid,
-major:estimoteSticker.major,minor:estimoteSticker.minor,
-type:estimoteSticker.type,firmware:estimoteSticker.firmware,
-bootloader:estimoteSticker.bootloader,temperature:estimoteSticker.temperature,
-moving:estimoteSticker.moving,batteryLevel:estimoteSticker.batteryLevel,
-x:estimoteSticker.acceleration.x,y:estimoteSticker.acceleration.y,
-z:estimoteSticker.acceleration.z,currentMotionStateDuration:estimoteSticker.currentMotionStateDuration,
-prevMotionStateDuration:estimoteSticker.previousMotionStateDuration,
-power:estimoteSticker.power,firmwareState:estimoteSticker.firmwareState,
-rssi:estimoteSticker.rssi,log_time:Date.now(),mac_id:macAddr};
+var values = "{
+				id:"+estimoteSticker.id+","+
+				"uuid:"+estimoteSticker.uuid+","+
+				"major:"+estimoteSticker.major+","+
+				"minor:"+estimoteSticker.minor+","+
+				"type:"+estimoteSticker.type+","+
+				"firmware:"+estimoteSticker.firmware+","+
+				"bootloader:"+estimoteSticker.bootloader+","+
+				"temperature:"+estimoteSticker.temperature+","+
+				"moving:"+estimoteSticker.moving+","+
+				"batteryLevel:"+estimoteSticker.batteryLevel+","+
+				"x:"+estimoteSticker.acceleration.x+","+
+				"y:"+estimoteSticker.acceleration.y+","+
+				"z:"+estimoteSticker.acceleration.z+","+
+				"currentMotionStateDuration:"+estimoteSticker.currentMotionStateDuration+","+
+				"prevMotionStateDuration:"+estimoteSticker.previousMotionStateDuration+","+
+				"power:"+estimoteSticker.power+","+
+				"firmwareState:"+estimoteSticker.firmwareState+","+
+				"rssi:"+estimoteSticker.rssi+","+
+				"log_time:"+Date.now()+","+
+				"mac_id:"+macAddr+"}";
 
-var queryString2 = connection.query('insert into estimote_stickers set ?',values,function(err,result){
-if (err) console.log(err);
-});
-/*console.log(queryString2.sql);*/
+				var sa = require('superagent');
+				sa.post('http://198.211.106.52/crc/saveEstimotedata')
+  				.send(values)
+  				.end(function(err, res) {
+    					console.log(err);
+    					console.log(res);
+  				});
 
-var queryString1 = 'SELECT * from estimote_stickers';
-connection.query(queryString1,function(err,rows,fields){
-		/*if (err) console.log(err);*/
-		for (var i in rows){
-		/*console.log(rows[i].solution);*/
-		} 
-})
-connection.end();
+
 });
+
 });
 EstimoteSticker.startScanning();
 
